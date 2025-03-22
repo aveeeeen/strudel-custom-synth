15626843962a4caf9f830f0b09fd9ec2df362b3a
aveeeeen <taichimatsumoto360@gmail.com>
2025-03-22 21:05:09 +0900

    finnaly found a way to pass audio node with tone.js properly

--- src/synthPreset/kick.js
+++ src/synthPreset/kick.js
@@ -32,11 +32,12 @@ export const useKick = () => {
   })
 
   // Let the gain node from OG Web Audio API as the destination source of your audio chain
-  // This way of connection is the only way that you can apply all the strudel's effect chain in combination with Tone.js
+  // This way of connection is the only way where you can apply all the strudel's effect chain in combination with Tone.js
   const endOfNode = new GainNode(getAudioContext(), {gain: 1})
   kick.connect(endOfNode)
 
-  //create parameters so that the strudel repl can interpret. These parameters will be passed as hap values to the sound registered in strudel.
+  //create parameters so that the strudel repl can interpret. These parameters (control patterns/parameters) will be passed as hap values to the sound registered in strudel.
+  //some control parameters are pre-defined so becareful
   createParams('harm', 'modu')
 
   registerSound(
@@ -47,11 +48,13 @@ export const useKick = () => {
       value, // The value of the `Hap` (passed values as control patterns. To able to pass these values to this function you need to assign it as a valid parameter from createParams() function)
       onended // A callback that should be fired when the sound has ended
     ) => {
+      
       let { decay, harm, modu, note, duration } = value;
       console.log(value)
 
       const freq = note ? getFreq(note) : "f2"
 
+      // map parameter values
       if (decay) {
         kick.set(
           {
@@ -78,9 +81,12 @@ export const useKick = () => {
         })
       }
 
+      // sound triggering
+
       kick.triggerAttack(freq, time)
       kick.triggerRelease(time + duration)
 
+      // this is a callback which is emmited when the sound stops
       const stop = (time) => {
         // disconnect only the end of node (endOfNode) 
         // the official strudel doc gives you an example where you disconnect all sources but if you do this with Tone.js Audio Node, you'll get no sound 
